824e3287c19866c205f22cd6c1bf6f482d17700d
tarosjk <tarosjk@gmail.com>
2023-06-02 12:01:53 +0900

    jQueryプロジェクト追加

--- jsonajax/js/jsonajax.js
+++ jsonajax/js/jsonajax.js
@@ -21,16 +21,33 @@
 const btn = document.querySelector('#btn')
 const info = document.querySelector('#car-info')
 const request = new XMLHttpRequest()
-request.open('get', 'https://norixx.github.io/jquery_elearing/cars1.json')
+let counter = 1
 
 btn.addEventListener('click', () => {
+  request.open('get', `https://norixx.github.io/jquery_elearing/cars${counter}.json`)
+
   request.onload = () => {
     console.log(request.responseText)
-    const res = JSON.parse(request.responseText)
-    outputHtml(res)
+    if (request.status === 200) {
+      const res = JSON.parse(request.responseText)
+      outputHtml(res)
+    } else {
+      console.log('サーバに接続しましたが、エラーとなりました')
+    }
+
+    if (counter <= 3) {
+      btn.removeAttribute('disabled')
+    }
+  }
+
+  request.onerror = () => {
+    console.log('接続エラーです')
+    btn.removeAttribute('disabled')
   }
 
   request.send()
+  btn.setAttribute('disabled', 'disabled')
+  counter++
 })
 
 // [{},{},{}]
